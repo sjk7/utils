730cc468a47cbad2e7dd115ac6a2f00250416b6a
steve mac <stevemac@steves-MacBook-Air.local>
2022-10-07 03:19:50 +0100

    Mac

---
+++ test_utils/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/stevekerr/Documents/utils/test_utils
+# Build directory: /Users/stevekerr/Documents/utils/test_utils/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ test_utils/test-utils.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#define UTILS_RUN_TESTS
+#include "../my_utils.hpp"
+
+int main(){
+    my::run_all_tests();
+}
