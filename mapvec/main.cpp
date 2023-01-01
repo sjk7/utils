c47d19a6b25b7893fde95638accc5202858da98d
s <radiowebmasters@gmail.com>
2023-01-01 02:41:41 +0000

    Concuded in windows that vector_map is OK so long as the keys are not strings

--- mapvec/main.cpp
+++ mapvec/main.cpp
@@ -5,322 +5,203 @@
 #include <random>
 #include <unordered_map>
 #include <algorithm>
+#include <numeric>
 
 using namespace std;
 
-void test_as_keys(const size_t);
-void test_strings(const size_t, bool sso = false);
+using intspair_t = std::pair<int, string>;
+std::vector<intspair_t> pairs;
+
+template <typename M, typename V>
+unsigned long add_to_map(M& m, V& pairs, string id) {
+
+    volatile int ctr = 0;
+    my::stopwatch sw(id);
+
+    m.reserve(pairs.size());
+    for (const auto& pr : pairs) {
+        auto ins = m.insert(pr);
+        assert(ins.second);
+        ctr++;
+    }
+
+    cout << "fake: " << ctr << endl;
+    return ctr;
+}
+
+auto add_to_vector_map(std::vector<intspair_t> pairs, const string& id) {
+    my::stopwatch sw(id);
+    return my::vector_map<int, string>(pairs.begin(), pairs.end());
+}
+
+auto add_to_unordered_map(std::vector<intspair_t> pairs, const string& id) {
+    my::stopwatch sw(id);
+    return std::unordered_map<int, string>(pairs.begin(), pairs.end());
+}
+
+template <typename T>
+unsigned long iterate_over(const T& m, const std::string& what) {
+
+    volatile unsigned long fake = 0;
+    my::stopwatch sw(what);
+    for (const auto thing : m) {
+        fake += thing.first;
+    }
+    cout << "fake: " << fake << endl;
+    return fake;
+}
+
+using stringvec_t = std::vector<std::pair<std::string, std::string>>;
+stringvec_t create_string_pairs(const size_t N) {
+
+    stringvec_t ret;
+    ret.reserve(N);
+    for (size_t i = 0; i < N; i++) {
+        std::string s = std::to_string(i);
+        ret.emplace_back(std::pair{s, s});
+    }
+    return ret;
+}
+
+template <typename T>
+unsigned long add_to_map(T& m, const stringvec_t& v, const std::string& what) {
+    my::stopwatch sw(what);
+    volatile unsigned long x = 0;
+    for (const auto& pr : v) {
+        auto inserted = m.insert(pr);
+        assert(inserted.second);
+        if (inserted.second) x = x + 1;
+    }
+
+    return x;
+}
+
+template <typename T>
+T create_map_from_strings(T& m, const stringvec_t& v, const std::string& what) {
+    my::stopwatch sw(what);
+    return T(v.begin(), v.end());
+}
+
+void test_using_strings(const size_t N) {
+
+    my::vector_map<string, string> vm;
+    std::unordered_map<string, string> um;
+    auto pairs = create_string_pairs(N);
+    cout << "Testing with " << N << " string pairs" << endl;
+
+    vm = create_map_from_strings(vm, pairs, "Creating vector_map from range");
+    um = create_map_from_strings(
+        um, pairs, "Creating unordered_map from range");
+
+    assert(vm.size() == um.size());
+    vm.clear();
+    um.clear();
+
+    unsigned long a
+        = add_to_map(um, pairs, "Adding string pairs to unordered_map");
+    unsigned long b
+        = add_to_map(vm, pairs, "Adding string pairs to vector_map");
+
+    assert(a == b);
+}
+
+void make_data(const size_t N) {
+
+    test_using_strings(N / 10);
+
+    std::vector<int> v(N);
+    std::iota(std::begin(v), std::end(v), 0);
+    pairs.reserve(N);
+    for (auto i : v) {
+        pairs.emplace_back(i, std::to_string(i));
+    }
+
+    my::vector_map<int, string> mv;
+    unordered_map<int, string> um;
+    add_to_map(um, pairs, "Adding to unordered_map");
+    add_to_map(mv, pairs, "Adding to vector_map");
+
+    assert(um.size() == mv.size());
+    assert(um.find(77)->second == "77");
+    assert(mv.find(77)->second == "77");
+
+    auto a = iterate_over(um, "Iterating over unordered_map");
+    auto b = iterate_over(mv, "Iterating over vector_map");
+    assert(a == b);
+
+    my::vector_map<int, string> mva;
+    unordered_map<int, string> uma;
+    uma = add_to_unordered_map(pairs, "Creating unordered_map from range");
+    cout << mv.size() << endl;
+    mva = add_to_vector_map(pairs, "Creating vector_map from range");
+    cout << mv.size() << endl;
+    assert(mva.size() == uma.size());
+
+    std::vector<intspair_t> with_dupes(pairs.begin(), pairs.end());
+    with_dupes.insert(with_dupes.end(), pairs.begin(), pairs.end());
+    assert(with_dupes.size() == pairs.size() * 2);
+
+    uma = add_to_unordered_map(
+        with_dupes, "Creating unordered_map from range (with dupes)");
+    cout << mv.size() << endl;
+    mva = add_to_vector_map(
+        with_dupes, "Creating vector_map from range (with dupes)");
+    cout << mv.size() << endl;
+    assert(mva.size() == uma.size());
+}
+
+template <typename M>
+void insert_into(M& m, std::pair<int, string> what, size_t expected_size,
+    bool expected_exist = false) {
+
+    auto pr = m.insert(what);
+    if (!expected_exist) {
+
+        assert(pr.second);
+        assert(pr.first != m.end());
+        assert((*pr.first).second == what.second);
+        assert((*pr.first).first == what.first);
+    } else {
+        assert(!pr.second);
+        assert(pr.first == m.find(what.first));
+    }
+
+    assert(m.size() == expected_size);
+    assert(!m.empty());
+}
+
 int main() {
-    cout << "Size of float: " << sizeof(float) << endl;
-    cout << "Size of double: " << sizeof(double) << endl;
-    SortedVector<int> sv;
-    auto insertResult = sv.insert(11);
-    assert(insertResult != sv.end());
-    // assert(insertResult.second);
-    assert(sv.size() == 1);
-    assert(sv.find(11) != sv.end());
-    assert(*(sv.find(11)) == 11);
-    auto ins = sv.insert(11);
-    // assert(ins.second == false);
-    assert(*ins == 11);
-    assert(sv.size() == 1);
-    cout << endl;
 
-    {
-        SortedVector<int, MultiKeysPolicy> sv;
-        auto ins = sv.insert(77);
-        assert(ins.Inserted() && ins.Where() != sv.end());
-        assert(sv.size() == 1);
-        ins = sv.insert(77);
-        assert(ins.Inserted() && ins.Where() != sv.end());
-        assert(sv.size() == 2);
-        auto it = std::equal_range(sv.begin(), sv.end(), 77);
-        assert(*it.first == 77);
-        ++it.first;
-        assert(*it.first == 77);
-        ++it.first;
-        assert(it.first == sv.end());
-    }
+    my::vector_map<int, string> vm;
+    unordered_map<int, string> um;
+    assert(vm.empty());
 
-#ifdef NDEBUG
-    static const int N = 100'000;
+    insert_into(vm, {1, "one"}, 1);
+    insert_into(um, {1, "one"}, 1);
 
-#else
-    static const int N = 50'000;
-#endif
+    insert_into(um, {1, "one"}, 1, true);
+    insert_into(vm, {1, "one"}, 1, true);
 
-    cout << "******************* TESTING " << N << " SHORT STRINGS"
-         << "*****************\n\n ";
-    test_strings(N, true);
-    cout << "******************* TESTING " << N << " LONG STRINGS"
-         << "*****************\n\n ";
-    test_strings(N, false);
-}
+    auto pr = um.find(1);
+    assert(pr->first == 1);
 
-std::vector<int> make_random_ints(const size_t N) {
-    std::random_device
-        rd; // Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd());
-    // Standard mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> distrib(0, (int)(N - 1));
-    std::vector<int> vals;
-    vals.reserve(N);
+    auto p = vm.find(1);
+    assert(p->first == 1);
 
-    for (int i = 0; i < N; i++) {
-        const auto r = i; // distrib(gen);
-        vals.push_back(r);
-    }
-    return vals;
-}
+    make_data(1000000);
 
-void test_strings(const size_t n, bool sso) {
-    using str_um_t = std::unordered_map<std::string, std::string>;
-    using sm_t = SteveMap<std::string, std::string>;
-    str_um_t um;
-    sm_t sm;
-    std::vector<std::string> random_strings;
-    random_strings.reserve(n);
-    cout << "Creating " << n << " random strings... " << endl;
-    for (int i = 0; i < n; ++i) {
-        random_strings.push_back(
-            my::utils::strings::random_string(sso ? 10 : 64));
-    }
-
-    cout << "Creating " << n << " random (unfindable) strings... " << endl;
-    std::vector<std::string> unfindable;
-    unfindable.reserve(n);
-    for (int i = 0; i < n; ++i) {
-        unfindable.push_back(my::utils::strings::random_string(sso ? 6 : 32));
-    }
-
-    {
-        int ctr = 0;
-        my::stopwatch sw("Adding items to unordered_map");
-        for (auto&& s : random_strings) {
-            um.insert({s, s});
-        }
-        assert(um.size() == random_strings.size());
-    }
-
-    {
-        int ctr = 0;
-        sm.reserve(random_strings.size());
-        my::stopwatch sw("Adding items to SteveMap");
-        // sm.insert_range(random_strings); <-- also works, but perhaps unfair
-        // comparison?
-        for (auto&& s : random_strings) {
-            auto ret = sm.insert({s, s});
-            assert(ret != sm.end());
-            ++ctr;
-        }
-        assert(sm.size() == random_strings.size());
-        assert(sm.values().size() == random_strings.size());
-        assert(sm.keys().size() == random_strings.size());
-    }
-
-    {
-        int ctr = 0;
-        sm.reserve(random_strings.size());
-        my::stopwatch sw(
-            "Adding existing items to SteveMap (nothing should happen)");
-        using pair_type = std::pair<std::string, std::string>;
-        std::vector<pair_type> v;
-        v.reserve(sm.size());
-        for (auto&& s : random_strings) {
-            pair_type p{s, s};
-            v.push_back(p);
-        }
-        sm.insert_range(v);
-
-        assert(sm.size() == random_strings.size());
-        assert(sm.values().size() == random_strings.size());
-        assert(sm.keys().size() == random_strings.size());
-    }
-
-    {
-        int ctr = 0;
-        my::stopwatch sw(
-            "Adding existing items to UnorderedMap (nothing should happen)");
-        // sm.insert_range(random_strings); <-- also works, but perhaps unfair
-        // comparison?
-        for (auto&& s : random_strings) {
-            um.insert({s, s});
-        }
-        assert(um.size() == random_strings.size());
-    }
-
-    // check they won't go in a second time:
-
-    cout << "Shuffling the keys so they are not in the order we added them to "
-            "the maps ..."
+    cout << endl
+         << endl
+         << "Conclude that vector_map is better than unordered_map, but only "
+            "if the keys are NOT strings!!"
+         << endl
+         << "Basically any type that is cheap to move is OK for the key, else "
+            "just use unordered_map : it is the insertions for strings that "
+            "kills it."
+         << endl
+         << "If you can create vector_map with a range rather than individual "
+            "insert()s, then strings might be OK"
          << endl;
-    auto rng = std::default_random_engine{};
-    std::shuffle(std::begin(random_strings), std::end(random_strings), rng);
 
-    auto ints = make_random_ints(n);
-    {
-        volatile int fake = 0;
-        my::stopwatch sw("Finding random (existing) keys in unordered_map");
-        for (auto i : ints) {
-            auto found = um.find(random_strings[i]);
-            assert(found != um.end());
-            if (found != um.end()) {
-                fake = fake + 1;
-            }
-        }
-        assert((size_t)fake == ints.size());
-        // cout << "fake output " << fake << endl;
-    }
-
-    {
-        int ctr = 0;
-        volatile int fake = 0;
-        my::stopwatch sw("Finding random (existing) keys in stevemap");
-        const auto& keys = sm.keys();
-        for (int i = 0; i < ints.size(); ++i) {
-            auto found = sm.find(keys[i].first);
-            assert(found != sm.end());
-            if (found != sm.end()) {
-
-                fake = fake + 1;
-            }
-            ++ctr;
-        }
-        assert((size_t)fake == ints.size());
-        // cout << "fake output " << fake << endl;
-    }
-
-    auto isize = (int)um.size();
-    {
-        volatile int fake = 0;
-        int idx = 0;
-
-        my::stopwatch sw("Finding random (non-existing) keys in unordered_map");
-        for (int i = isize; i < isize + n; i++) {
-            auto found = um.find(unfindable[idx++]);
-            if (found != um.end()) {
-                fake = fake + 1;
-            }
-        }
-        assert((size_t)fake == 0);
-        // cout << "fake output " << fake << endl;
-    }
-
-    {
-        volatile int fake = 0;
-        int idx = 0;
-        my::stopwatch sw("Finding random (non-existing) keys in Stevemap");
-        for (int i = isize; i < isize + n; i++) {
-            auto found = sm.find(unfindable[idx++]);
-            if (found != sm.end()) {
-                fake = fake + 1;
-            }
-        }
-        assert((size_t)fake == 0);
-        // cout << "fake output " << fake << endl;
-    }
+    return 0;
 }
-
-template <typename T> void add_stuff(T& t, std::string what, const int N) {
-    my::stopwatch sw(what);
-    t.reserve(N);
-
-    for (int i = 0; i < N; i++) {
-        t.insert({i, i});
-    }
-}
-
-template <typename K, typename V>
-void add_stuff(SteveMap<K, V>& t, std::string what, const int N) {
-    my::stopwatch sw(what);
-    t.reserve(N);
-
-    std::random_device
-        rd; // Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd());
-    // Standard mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> distrib(0, N - 1);
-    std::vector<V> vals;
-    vals.reserve(N);
-
-    for (int i = 0; i < N; i++) {
-        const auto r = i; // distrib(gen);
-        vals.push_back(r);
-    }
-
-    t.insert_range(vals);
-}
-
-template <typename K, typename V>
-void test_find(std::unordered_map<K, V>& m, SteveMap<K, V>& sm) {
-    const int N = m.size();
-    std::random_device
-        rd; // Will be used to obtain a seed for the random number engine
-    std::mt19937 gen(rd());
-    // Standard mersenne_twister_engine seeded with rd()
-    std::uniform_int_distribution<> distrib(0, m.size() - 1);
-    volatile unsigned int fake_total = 0;
-
-    {
-        my::stopwatch sw("Finding randomly in " + std::to_string(N)
-            + " unordered_map items");
-        for (int i = 0; i < N; i++) {
-            int r = distrib(gen);
-            auto itfound = m.find(r);
-            assert(itfound != m.end());
-            auto v = *itfound;
-            fake_total += v.second;
-        }
-    }
-
-    // cout << fake_total << '\r';
-    {
-        my::stopwatch sw(
-            "Finding randomly in " + std::to_string(N) + " SteveMap items");
-        for (int i = 0; i < N; i++) {
-            int r = distrib(gen);
-            auto pr = sm.find(r);
-            auto v = pr.second;
-            fake_total += v;
-        }
-    }
-    // cout << fake_total << '\r';
-}
-
-template <typename T> void iterate_over(const T& t, const char* what) {
-    volatile unsigned long fake = 0;
-    my::stopwatch sw(what);
-    for (const auto& pr : t) {
-        fake += pr.second;
-    }
-    // cout << "iterator fake is: " << '\r';
-    //;
-}
-
-template <typename T> void map_sanity(const T& t) {
-    volatile unsigned long fake = 0;
-
-    for (const auto& pr : t) {
-        bool b = pr.first == pr.second;
-        assert(b);
-        fake += b;
-    }
-
-    // std::cout << fake << '\r';
-    //;
-}
-
-/*/
-    void test_as_keys(size_t N) {
-        SteveMap<int, int> v;
-        std::unordered_map<int, int> m;
-
-        add_stuff(v, "adding " + std::to_string(N) + " to SteveMap", N);
-        add_stuff(m, "adding " + std::to_string(N) + " to map", N);
-
-        test_find(m, v);
-        iterate_over(v, "Iterate over SteveMap");
-        iterate_over(m, "Iterate over unordered_map");
-        map_sanity(v);
-    }
-    /*/
--- mapvec/mapvec.h
+++ mapvec/mapvec.h
@@ -6,210 +6,112 @@
 #include <vector>
 #include <type_traits> // false_type
 #include <utility> // std::pair
+#include <iterator>
 
-template <typename I> struct find_result {
-    find_result(I where, bool found) : where(where), found(found) {}
+namespace my {
+
+template <typename I> struct inserted_return_type {
     I where;
-    bool found = false;
+    bool inserted;
+    inserted_return_type(I where, bool inserted)
+        : where(where), inserted(inserted) {}
 };
-
-template <typename VECITER> struct InsertResult {
-    InsertResult(VECITER& where, bool didInsert = false)
-        : inserted(didInsert), where(where) {}
-    bool Inserted() const noexcept { return inserted; }
-    VECITER Where() const noexcept { return where; }
-
+template <class T, class Compare = std::less<T>, class UNIQUE = std::true_type>
+struct sorted_vector {
     private:
-    bool inserted = false;
-    VECITER where;
-};
-template <typename> struct is_pair : std::false_type {};
+    // using std::lower_bound;
+    // using std::vector;
 
-template <typename T, typename U>
-struct is_pair<std::pair<T, U>> : std::true_type {};
-
-struct MultiKeysPolicy {
-    template <typename VEC, typename K> static auto insert(VEC& v, const K& k) {
-        auto it = std::lower_bound(v.begin(), v.end(), k);
-        it = v.insert(it, k);
-        return InsertResult{it, true};
-    }
-};
-
-struct UniqueKeysPolicy {
-
-    template <typename VEC, typename K>
-    // returns the same as unordered_map would
-    static inline auto insert(VEC& v, K&& k) {
-        auto it = find(v, k);
-
-        if (it == v.end()) { // ie if it is not ALREADY in the vector
-            // pair<iterator,bool>
-            auto rv = v.insert(it, k);
-            return rv;
-        }
-
-        if constexpr (is_pair<typename VEC::value_type>::value) {
-            if (it->first != k.first) {
-                auto rv = v.insert(it, k);
-                // auto ret = std::pair{rv, true};
-                return rv;
-            }
-        } else {
-
-            if (*it != k) {
-                auto rv = v.insert(it, k);
-                auto ret = std::make_pair(rv, true);
-                return it;
-            } else {
-                return it;
-            }
-        }
-
-        return v.end();
-    }
-    template <typename VEC, typename K> static auto find(VEC& v, const K& k) {
-
-        if constexpr (is_pair<typename VEC::value_type>::value) {
-
-            auto it = std::lower_bound(
-                v.begin(), v.end(), k, [](const auto& a, const auto& b) {
-                    bool ret = a.first < b.first;
-                    return ret;
-                });
-            return it;
-
-        } else {
-            auto it = std::lower_bound(
-                v.begin(), v.end(), k, [](const auto& a, const auto& b) {
-                    bool ret = a < b;
-                    return ret;
-                });
-            return it;
-        }
-    }
-};
-
-template <typename T, typename DUPES_POLICY = UniqueKeysPolicy>
-class SortedVector : private std::vector<T> {
-    using base = std::vector<T>;
-    DUPES_POLICY dupesPolicy = DUPES_POLICY{};
+    std::vector<T> V;
 
     public:
-    using cit = typename base::const_iterator;
-    using it = typename base::iterator;
-    using base::operator[];
+    void reserve(size_t N) { V.reserve(N); }
+    size_t size() const noexcept { return V.size(); }
+    bool empty() const noexcept { return V.empty(); }
 
-    using base::cbegin;
-    using base::cend;
-    using base::clear;
-    using base::data;
+    sorted_vector(const Compare& c = Compare()) : V(), cmp(c) {}
+    template <class InputIterator>
+    sorted_vector(
+        InputIterator first, InputIterator last, const Compare& c = Compare())
+        : V(first, last), cmp(c) {
+        std::sort(begin(), end(), cmp);
+        if constexpr (UNIQUE::value)
+            V.erase(std::unique(V.begin(), V.end()), V.end());
+    }
+    virtual ~sorted_vector() = default;
+    void clear() { V.clear(); }
+    Compare cmp;
+    typedef typename std::vector<T>::iterator iterator;
+    typedef typename std::vector<T>::const_iterator const_iterator;
+    iterator begin() { return V.begin(); }
+    iterator end() { return V.end(); }
+    const_iterator begin() const { return V.begin(); }
+    const_iterator end() const { return V.end(); }
+    using insert_return_type = inserted_return_type<iterator>;
 
-    using base::reserve;
-    using base::resize;
-    using base::size;
-    cit begin() const noexcept { return base::cbegin(); }
-
-    cit end() const noexcept { return base::cend(); }
-    auto begin() { return base::begin(); }
-    auto end() { return base::end(); }
-
-    auto insert(const T& val) {
-        // auto where = std::lower_bound(base::begin(), base::end(), val);
-        // auto it = base::insert(where, val);
-        base& b = *this;
-        auto where = dupesPolicy.insert(b, val);
-        return where;
+    insert_return_type insert(const T& t) {
+        iterator i = std::lower_bound(begin(), end(), t, cmp);
+        if (i == end() || cmp(t, *i)) {
+            i = V.insert(i, t);
+            return insert_return_type{i, true};
+        }
+        return insert_return_type{i, false};
     }
 
-    auto insert(T&& val) {
-        base& b = *this;
-        auto where = dupesPolicy.insert(b, std::forward<T>(val));
-        return where;
+    const_iterator find(const T& t) const {
+        const_iterator i = std::lower_bound(begin(), end(), t, cmp);
+        return i == end() || cmp(t, *i) ? end() : i;
     }
-
-    auto find(const T& val) const noexcept {
-        return dupesPolicy.find(*this, val);
-    }
-    auto find(const T& val) noexcept { return dupesPolicy.find(*this, val); }
-
-    auto push_back(const T& t) { return base::push_back(t); }
 };
 
-template <typename K, typename V> class SteveMap {
-    private:
-    using internal_pair_type = std::pair<K, size_t>; //
-    SortedVector<internal_pair_type>
-        m_keys; // find by key, the second member of the pair is the index
-                // of the value in values
-    std::vector<V> m_values;
+template <typename K, typename V> struct key_value_compare {
+    bool operator()(const auto& a, const auto& b) const {
+        return a.first < b.first;
+    }
+};
 
-    public:
-    using pair_type = std::pair<K, V>;
-    using values_type = std::vector<V>;
-    using iterator_type = typename values_type::iterator;
-    using insert_return_type = std::pair<iterator_type, bool>;
-    const SortedVector<internal_pair_type>& keys() const { return m_keys; }
-    const std::vector<V>& values() const { return m_values; }
+template <typename K, typename V>
+using key_value_pair_type = typename std::pair<K, V>;
 
-    auto size() const noexcept { return m_values.size(); }
+template <class K, class V, class Compare = key_value_compare<K, V>>
+struct vector_map
+    : sorted_vector<key_value_pair_type<K, V>, key_value_compare<K, V>> {
 
-    auto find_multiple(const K& key) {
-        auto pr = std::make_pair(key, key);
-        auto retval
-            = std::equal_range(m_keys.real_begin(), m_keys.real_end(), pr);
-        return retval;
+    using value_type = key_value_pair_type<K, V>;
+    using base_type = typename sorted_vector<key_value_pair_type<K, V>,
+        key_value_compare<K, V>>;
+
+    using key_type = K;
+    using val_type = V;
+    key_value_compare<K, V> cmp = key_value_compare<K, V>();
+    using const_iterator = base_type::const_iterator;
+    using iterator = base_type::iterator;
+
+    using insert_ret_t = std::pair<iterator, bool>;
+
+    template <typename InputIterator>
+    vector_map(InputIterator first, InputIterator last)
+        : base_type(first, last, cmp) {}
+
+    vector_map() : base_type(cmp) {}
+
+    auto insert(value_type keyval) {
+
+        auto irt = base_type::insert(keyval);
+        if (irt.inserted) {
+            return std::pair{irt.where, true};
+        }
+        return std::pair{irt.where, false};
     }
 
+    // to emulate unordered_map, we need to be able to find ONLY by key:
     auto find(const K& key) {
-        auto it = m_keys.find({key, 0});
-        if (it != m_keys.end() && it->first == key) {
-            return it;
-        }
-        return m_keys.end();
+        std::pair f{key, V()};
+        auto found = base_type::find(f);
+        return found;
     }
 
-    void insert_range(const std::vector<pair_type>& key_value_pairs) {
-        const auto old_size = m_values.size();
-        m_values.reserve(m_values.size() + key_value_pairs.size());
-        m_keys.reserve(m_keys.size() + key_value_pairs.size());
-        for (const auto& v : key_value_pairs) {
-            insert(v);
-        }
-    }
-
-    /*/
-    void index_all() {
-        // std::string s{"indexing all "};
-        //  s += std::to_string(values.size());
-        //  my::stopwatch sw(s);
-        m_keys.clear();
-        m_keys.resize(m_values.size());
-
-        size_t idx = 0;
-        for (auto&& v : m_values) {
-            auto pr = std::pair{idx, v};
-            m_keys[idx] = pr;
-            ++idx;
-        }
-    }
-    /*/
-
-    auto insert(const pair_type& pr) noexcept {
-        const auto pos = m_values.size();
-        auto insert_result = m_keys.insert({pr.first, pos});
-        if (insert_result != m_keys.end()) {
-            m_values.push_back(pr.second);
-        }
-        assert(m_keys.size() == m_values.size());
-        return insert_result;
-    }
-
-    void reserve(size_t sz) {
-        m_keys.reserve(sz);
-        m_values.reserve(sz);
-    }
-
-    auto begin() const noexcept { return m_keys.begin(); }
-    auto end() const noexcept { return m_keys.end(); }
+    void reserve(size_t N) { base_type::reserve(N); }
+    void clear() { base_type::clear(); }
 };
+} // namespace my
