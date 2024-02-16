b71b0aec9e589a1c422fde1498d90d51698166d9
SDRDesk <cppguyuk@gmail.com>
2024-02-16 06:22:51 +0000

    clang-tidy fixes

--- my_mem_tests/my_mem_utils_perf.cpp
+++ my_mem_tests/my_mem_utils_perf.cpp
@@ -9,150 +9,165 @@
 using namespace std;
 using namespace my;
 
-std::vector<std::string> make_random_strings(size_t howMany,
-                                             size_t string_len = 32) {
-  std::vector<std::string> strings;
-  printf("Making %zu random strings of length: %zu ...\n", howMany, string_len);
-  strings.reserve(howMany);
-  for (size_t i = 0; i < howMany; ++i) {
-    strings.push_back(utils::strings::random_string(string_len));
-  }
-  printf("Making %zu random strings, complete.\n", howMany);
-  return strings;
+std::vector<std::string> make_random_strings(size_t howMany, size_t string_len = 32)
+{
+    std::vector<std::string> strings;
+    printf("Making %zu random strings of length: %zu ...\n", howMany, string_len);
+    strings.reserve(howMany);
+    for (size_t i = 0; i < howMany; ++i)
+    {
+        strings.push_back(utils::strings::random_string(string_len));
+    }
+    printf("Making %zu random strings, complete.\n", howMany);
+    return strings;
 }
 
-auto test_arena(const std::vector<std::string> &v, Arena **a, bool &reset) {
-  volatile uint64_t ret = 0;
-  const auto start_time = my::stopwatch::now_ms();
+auto test_arena(const std::vector<std::string> &v, Arena **a, bool &reset)
+{
+    volatile uint64_t ret = 0;
+    const auto start_time = my::stopwatch::now_ms();
 
-  auto l = (size_t)0;
-  for (const auto &s : v) {
-    l = s.size();
-    volatile char *ptr = (char *)(*a)->alloc(l + 1);
-    if (!ptr) {
-      printf("******* Arena out of space. Only %zu bytes free. Resetting it. "
-             "*******\n",
-             (*a)->space());
-      reset = true;
-      // delete (*a);
-      //(*a) = new Arena();
-      (*a)->reset();
-      continue;
-    }
-    if (ptr)
-      memcpy((char *)ptr, s.c_str(), l + 1);
-    ret += strlen((char*)ptr); // need this to avoid optimising away
-  };
+    auto l = (size_t)0;
+    for (const auto &s : v)
+    {
+        l = s.size();
+        volatile char *ptr = (char *)(*a)->alloc(l + 1);
+        if (!ptr)
+        {
+            printf("******* Arena out of space. Only %zu bytes free. Resetting it. "
+                   "*******\n",
+                   (*a)->space());
+            reset = true;
+            // delete (*a);
+            //(*a) = new Arena();
+            (*a)->reset();
+            continue;
+        }
+        if (ptr != nullptr)
+        {
+            memcpy((char *)ptr, s.c_str(), l + 1);
+        }
+        ret += strlen((char *)ptr); // need this to avoid optimising away
+    };
 
-  // printf("space() in Arena ================== %zu\n", (*a)->space());
-  return my::stopwatch::now_ms() - start_time;
+    // printf("space() in Arena ================== %zu\n", (*a)->space());
+    return my::stopwatch::now_ms() - start_time;
 }
 
-auto test_malloc(const std::vector<std::string> &v) {
+auto test_malloc(const std::vector<std::string> &v)
+{
 
-  volatile uint64_t ret = 0;
-  const auto start_time = my::stopwatch::now_ms();
-  char *firstptr = 0;
-  for (const auto &s : v) { // NOLINT
+    volatile uint64_t ret = 0;
+    const auto start_time = my::stopwatch::now_ms();
+    char *firstptr = nullptr;
+    for (const auto &s : v)
+    { // NOLINT
 
-    const auto l = s.size();
-    volatile char *ptr = (char *)malloc(l + 1); // NOLINT
-    memcpy((void *)ptr, s.c_str(), l + 1);
-    ret -= strlen((char*)ptr); // do not optimise me away!
-    if (firstptr == 0)
-      firstptr = (char *)ptr;
-  }
-  // free(firstptr); <-- Arena is not free'ing every time, and neither are we
-  return my::stopwatch::now_ms() - start_time;
+        const auto l = s.size();
+        volatile char *ptr = (char *)malloc(l + 1); // NOLINT
+        memcpy((void *)ptr, s.c_str(), l + 1);
+        ret -= strlen((char *)ptr); // do not optimise me away!
+        if (firstptr == nullptr)
+        {
+            firstptr = (char *)ptr;
+        }
+    }
+    // free(firstptr); <-- Arena is not free'ing every time, and neither are we
+    return my::stopwatch::now_ms() - start_time;
 }
 
-int main() {
-  puts("App started. It deliberately leaks memory.\nDon't worry about "
-       "it!\nPlease wait ...");
-  auto v = make_random_strings(3'000'000);
+int main()
+{
+    puts("App started. It deliberately leaks memory.\nDon't worry about "
+         "it!\nPlease wait ...");
+    auto v = make_random_strings(3'000'000);
 
-  int64_t arena_time = 0;
-  int64_t malloc_time = 0;
-  uint64_t ret = 0;
-  int64_t notify_period_ms = 250;
-  auto time_marker = my::stopwatch::now_ms();
+    int64_t arena_time = 0;
+    int64_t malloc_time = 0;
+    uint64_t ret = 0;
+    int64_t notify_period_ms = 250;
+    auto time_marker = my::stopwatch::now_ms();
 
-  std::string dot = ".";
-  Arena *a;
-  a = new Arena();
+    std::string dot = ".";
+    Arena *a = nullptr;
+    a = new Arena();
 
-  int arena_reset_at = 0;
-  int loop_max = 20;
-  bool reset = false;
-  for (int i = 0; i < loop_max; ++i) {
-    if (i == loop_max - 1 && arena_reset_at == 0) {
-      puts("Adding more iterations: arena has not yet reset()");
-      loop_max += 20;
+    int arena_reset_at = 0;
+    int loop_max = 20;
+    bool reset = false;
+    for (int i = 0; i < loop_max; ++i)
+    {
+        if (i == loop_max - 1 && arena_reset_at == 0)
+        {
+            puts("Adding more iterations: arena has not yet reset()");
+            loop_max += 20;
+        }
+        if (my::stopwatch::now_ms() - notify_period_ms >= time_marker)
+        {
+            time_marker = my::stopwatch::now_ms();
+            printf("%s\n", dot.c_str());
+            dot += '.';
+        }
+        arena_time += test_arena(v, &a, reset);
+        if (reset)
+        {
+            if (arena_reset_at == 0)
+            {
+                puts("Will make sure arena resets again, at least one more time");
+                arena_reset_at = i;
+                loop_max = i + arena_reset_at + 2;
+            }
+        }
+        // printf("ret a: %zu\n", (size_t)ret);
+
+        malloc_time += test_malloc(v);
+        // printf("ret b: %zu\n", (size_t)ret);
     }
-    if (my::stopwatch::now_ms() - notify_period_ms >= time_marker) {
-      time_marker = my::stopwatch::now_ms();
-      printf("%s\n", dot.c_str());
-      dot += '.';
+
+    puts(" ---------- Previous results, for comparison --------");
+    printf("\nARM64 build, Win11 UTM VM on Apple "
+           "Silicon:   Arena 1.5-2x\n");
+    printf("ARM64 build,  in MACOS on Apple "
+           "Silicon:   Arena 1.8-2.2x +\n");
+
+    printf("x86_64 build, in MACOS on Apple "
+           "Silicon (native):   Arena 1.8x +\n");
+
+    puts("\n\n ------------------- Results -------------------");
+    printf("Total Arena   execution time (ms): %lld\n", arena_time);
+    printf("Total malloc  execution time (ms): %lld\n", malloc_time);
+    float ratio = 1;
+    if (malloc_time < arena_time)
+    {
+        ratio = (float)arena_time / (float)malloc_time;
+        printf("Malloc faster than arena (%fx)\n", ratio);
     }
-    arena_time += test_arena(v, &a, reset);
-    if (reset) {
-      if (arena_reset_at == 0) {
-        puts("Will make sure arena resets again, at least one more time");
-        arena_reset_at = i;
-        loop_max = i + arena_reset_at + 2;
-      }
+    else
+    {
+        ratio = (float)malloc_time / (float)arena_time;
+        printf("Arena faster than malloc (%fx)\n", ratio);
     }
-    // printf("ret a: %zu\n", (size_t)ret);
 
-    malloc_time += test_malloc(v);
-    // printf("ret b: %zu\n", (size_t)ret);
-  }
-
-  puts(" ---------- Previous results, for comparison --------");
-  printf(
-      "\nARM64 build, Win11 UTM VM on Apple "
-      "Silicon:   Arena 1.5-2x\n");
-  printf(
-      "ARM64 build,  in MACOS on Apple "
-      "Silicon:   Arena 1.8-2.2x +\n");
-
- printf(
-      "x86_64 build, in MACOS on Apple "
-      "Silicon (native):   Arena 1.8x +\n");
-
-  puts("\n\n ------------------- Results -------------------");
-  printf("Total Arena   execution time (ms): %lld\n", arena_time);
-  printf("Total malloc  execution time (ms): %lld\n", malloc_time);
-  float ratio = 1;
-  if (malloc_time < arena_time) {
-    ratio = (float)arena_time / (float)malloc_time;
-    printf("Malloc faster than arena (%fx)\n", ratio);
-  } else {
-    ratio = (float)malloc_time / (float)arena_time;
-    printf("Arena faster than malloc (%fx)\n", ratio);
-  }
-  if (a)
     delete a;
 
+    printf("\nAll done!\n");
+    return (int)ret;
 
-  printf("\nAll done!\n");
-  return (int)ret;
+    /*/
+    Arena myArena;
+    printf("Arena capacity() is %zu bytes\n", myArena.capacity());
+    assert(myArena.capacity() > 0);
 
-  /*/
-  Arena myArena;
-  printf("Arena capacity() is %zu bytes\n", myArena.capacity());
-  assert(myArena.capacity() > 0);
+    size_t old_free = myArena.space();
+    char* strptr = (char*)myArena.alloc(256);
+    size_t new_free = myArena.space();
+    assert(old_free - new_free == 256);
+    assert(myArena.size() == 256);
+    constexpr const char* greet = "Hello, nobba!";
+    memcpy(strptr, greet, strlen(greet));
 
-  size_t old_free = myArena.space();
-  char* strptr = (char*)myArena.alloc(256);
-  size_t new_free = myArena.space();
-  assert(old_free - new_free == 256);
-  assert(myArena.size() == 256);
-  constexpr const char* greet = "Hello, nobba!";
-  memcpy(strptr, greet, strlen(greet));
-
-  assert(strptr);
-  puts(strptr);
-  /*/
-  return 0;
+    assert(strptr);
+    puts(strptr);
+    /*/
+    return 0;
 }
--- my_memory_utils.hpp
+++ my_memory_utils.hpp
@@ -2,9 +2,7 @@
 #define MY_MEMORY_UTILS_HPP
 
 #include "./my_utils.hpp"
-
-namespace my {
-#include <string.h>
+#include <cstring>
 
 #ifndef _WIN32
 #include <sys/mman.h>
@@ -12,90 +10,124 @@ namespace my {
 #include <Windows.h>
 #endif
 
+namespace my
+{
+
 // https://github.com/MicrosoftDocs/win32/blob/docs/desktop-src/Memory/reserving-and-committing-memory.md
 // about 4 times the speed of malloc()
 // under load on Mac Silicon.
-class Arena {
+class Arena
+{
 
-  char *m_ptr = nullptr;
-  char *m_begin = nullptr;
-  char *m_end = nullptr;
-  void mapmem(size_t cap) {
+    char *m_ptr = nullptr;
+    char *m_begin = nullptr;
+    char *m_end = nullptr;
+    void mapmem(size_t cap)
+    {
 
-    if (m_ptr)
-      unmap();
+        if (m_ptr != nullptr)
+        {
+            unmap();
+        }
 
-    int errCode = 0;
+        int errCode = 0;
 #ifndef _WIN32
-    int flags = PROT_READ | PROT_WRITE;
-    int types = MAP_PRIVATE | MAP_ANONYMOUS;
+        int flags = PROT_READ | PROT_WRITE;
+        int types = MAP_PRIVATE | MAP_ANONYMOUS;
 
-    m_begin = (char *)mmap(nullptr, cap, flags, types, 0, 0);
-    if (!m_begin)
-      errCode = errno;
+        m_begin = (char *)mmap(nullptr, cap, flags, types, 0, 0);
+        if (m_begin == nullptr)
+        {
+            errCode = errno;
+        }
 #else
-    m_begin =
-        (char *)VirtualAlloc(0, cap, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
-    if (!m_begin)
-      errCode = GetLastError();
+        m_begin = (char *)VirtualAlloc(0, cap, MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+        if (!m_begin)
+            errCode = GetLastError();
 
-    assert(m_begin);
+        assert(m_begin);
 
 #endif
-    if (errCode) {
-      const auto s = my::utils::strings::system_error_string(errCode);
-      if (!s.empty())
-        fprintf(stderr, "VirtualAlloc failed: %s\n", s.c_str());
+        if (errCode != 0)
+        {
+            const auto s = my::utils::strings::system_error_string(errCode);
+            if (!s.empty())
+                fprintf(stderr, "VirtualAlloc failed: %s\n", s.c_str());
+        }
+        m_ptr = m_begin;
+        m_end = m_begin + cap;
     }
-    m_ptr = m_begin;
-    m_end = m_begin + cap;
-  }
-  void unmap() {
+    void unmap()
+    {
 #ifndef _WIN32
-    if (m_begin)
-      munmap(m_begin, capacity());
+        if (m_begin != nullptr)
+        {
+            munmap(m_begin, capacity());
+        }
 #else
-    if (m_begin) {
-      BOOL bSuccess = VirtualFree(m_begin,      // Base address of block
-                                  0,            // Bytes of committed pages
-                                  MEM_RELEASE); // Decommit the pages
-      assert(bSuccess);
-    }
+        if (m_begin)
+        {
+            BOOL bSuccess = VirtualFree(m_begin,      // Base address of block
+                                        0,            // Bytes of committed pages
+                                        MEM_RELEASE); // Decommit the pages
+            assert(bSuccess);
+        }
 #endif
-    m_begin = 0;
-    m_end = 0;
-    m_ptr = 0;
-  }
-
-public:
-  Arena(size_t capacity = 4 * GB) noexcept : m_ptr(0), m_begin(0), m_end(0) {
-    mapmem(capacity);
-  }
-  ~Arena() { unmap(); }
-  void *alloc(size_t sz) {
-    void *ret = (void *)m_ptr;
-    char *newptr = m_ptr + sz;
-    if (newptr >= m_end) {
-      return nullptr;
+        m_begin = nullptr;
+        m_end = nullptr;
+        m_ptr = nullptr;
     }
-    m_ptr += sz;
-    return ret;
-  }
-  static constexpr inline size_t KB = 1024;
-  static constexpr inline size_t MB = KB * 1024;
-  static constexpr inline size_t GB = MB * 1024;
 
-public:
-  void *begin() const noexcept { return (void *)m_begin; }
-  void *end() const noexcept { return m_end; }
-  size_t capacity() const noexcept { return m_end - m_begin; }
-  size_t space() const noexcept { return m_end - m_ptr; }
-  size_t size() const noexcept { return m_ptr - m_begin; }
-  void reset() noexcept {
-    // cannot simply m_ptr = m_begin as it's super-slow to memcpy after a reset
-    // in MACOS.
-    mapmem(capacity());
-  }
+  public:
+    Arena(size_t capacity = 4 * GB) noexcept : m_ptr(nullptr), m_begin(nullptr), m_end(nullptr)
+    {
+        mapmem(capacity);
+    }
+    ~Arena()
+    {
+        unmap();
+    }
+    void *alloc(size_t sz)
+    {
+        void *ret = (void *)m_ptr;
+        char *newptr = m_ptr + sz;
+        if (newptr >= m_end)
+        {
+            return nullptr;
+        }
+        m_ptr += sz;
+        return ret;
+    }
+    static constexpr inline size_t KB = 1024;
+    static constexpr inline size_t MB = KB * 1024;
+    static constexpr inline size_t GB = MB * 1024;
+
+    [[nodiscard]] void *begin() const noexcept
+    {
+        return (void *)m_begin;
+    }
+    [[nodiscard]] void *end() const noexcept
+    {
+        return m_end;
+    }
+    [[nodiscard]] size_t capacity() const noexcept
+    {
+        return m_end - m_begin;
+    }
+    [[nodiscard]] size_t space() const noexcept
+    {
+        return m_end - m_ptr;
+    }
+    [[nodiscard]] size_t size() const noexcept
+    {
+        return m_ptr - m_begin;
+    }
+    void reset() noexcept
+    {
+        // cannot simply m_ptr = m_begin as it's super-slow to memcpy after a reset
+        // in MACOS.
+        mapmem(capacity());
+    }
 };
 } // namespace my
 
